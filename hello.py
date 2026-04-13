5000492d4f7228e4cf2170e3ce2318377cb5ee9e
ECode09 <EAJohnson067@gmail.com>
2026-04-13 14:32:25 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello, World!");
+# Eli Johnson
+# 4/13/2026
+# A simple Python program to print a message
+
+# Eli Johnson
+# 4/13/2026
+# Added new message
+
+print("Hello, World!")
+
+print ("Welcome to GitHub!")
